a57ec0d2791749cde5d2cc0defdf62252c037e1e
Akbota1991 <a.nasvetova91@gmail.com>
2023-04-17 15:21:56 -0600

    added us02

--- src/test/java/com/library/steps/APIStepDefs.java
+++ src/test/java/com/library/steps/APIStepDefs.java
@@ -11,6 +11,9 @@ import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import org.hamcrest.Matchers;
+import org.junit.Assert;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
@@ -20,9 +23,10 @@ public class APIStepDefs {
     RequestSpecification givenPart;
     Response response;
     ValidatableResponse thenPart;
+
+
     /**
      * US 01 RELATED STEPS
-     *
      */
     @Given("I logged Library api as a {string}")
     public void i_logged_library_api_as_a(String userType) {
@@ -30,6 +34,7 @@ public class APIStepDefs {
         givenPart = given().log().uri()
                 .header("x-library-token", LibraryAPI_Util.getToken(userType));
     }
+
     @Given("Accept header is {string}")
     public void accept_header_is(String contentType) {
         givenPart.accept(contentType);
@@ -40,17 +45,49 @@ public class APIStepDefs {
         response = givenPart.when().get(ConfigurationReader.getProperty("library.baseUri") + endpoint).prettyPeek();
         thenPart = response.then();
     }
+
     @Then("status code should be {int}")
     public void status_code_should_be(Integer statusCode) {
         thenPart.statusCode(statusCode);
     }
+
     @Then("Response Content type is {string}")
     public void response_content_type_is(String contentType) {
         thenPart.contentType(contentType);
     }
+
     @Then("{string} field should not be null")
     public void field_should_not_be_null(String path) {
         thenPart.body(path, everyItem(notNullValue()));
     }
+    /**
+     * US 02 Bota  RELATED STEPS
+     */
+    static String id;
 
+
+    @Given("Path param is {string}")
+    public void path_param_is(String id) {
+
+        this.id = id;
+        givenPart.pathParam("id", id);
+
+    }
+
+    @Then("{string} field should be same with path param")
+    public void field_should_be_same_with_path_param(String path) {
+
+        String path1 = thenPart.extract().jsonPath().getString(path);
+
+        Assert.assertEquals(path1, id);
+
+    }
+
+    @Then("following fields should not be null")
+    public void following_fields_should_not_be_null(List<String> dataList) {
+
+        for (String eachField : dataList) {
+            thenPart.body(eachField, is(notNullValue()));
+        }
+    }
 }
